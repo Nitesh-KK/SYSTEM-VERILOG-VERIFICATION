a842dc70d92bf5dbcb0b1143e47708d666f3b9ff
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-06 19:25:52 +0530

    Create 12. Output.sv

---
+++ Half_Adder/12. Output.sv
@@ -0,0 +1,49 @@
+xcelium> run
+Generator
+Time=0 a=0 b=0 sum=0 carry=0
+Driver
+Time=1 a=0 b=0 sum=0 carry=0
+Monitor
+Time=2 a=0 b=0 sum=0 carry=0
+Scoreboard
+Time=2 a=0 b=0 sum=0 carry=0
+-----PASS-----
+Generator
+Time=3 a=0 b=1 sum=0 carry=0
+Driver
+Time=4 a=0 b=1 sum=0 carry=0
+Monitor
+Time=5 a=0 b=1 sum=1 carry=0
+Scoreboard
+Time=5 a=0 b=1 sum=1 carry=0
+-----PASS-----
+Generator
+Time=6 a=1 b=1 sum=0 carry=0
+Driver
+Time=7 a=1 b=1 sum=0 carry=0
+Monitor
+Time=8 a=1 b=1 sum=0 carry=1
+Scoreboard
+Time=8 a=1 b=1 sum=0 carry=1
+-----PASS-----
+Generator
+Time=9 a=0 b=0 sum=0 carry=0
+Driver
+Time=10 a=0 b=0 sum=0 carry=0
+Monitor
+Time=11 a=0 b=0 sum=0 carry=0
+Scoreboard
+Time=11 a=0 b=0 sum=0 carry=0
+-----PASS-----
+Generator
+Time=12 a=1 b=0 sum=0 carry=0
+Driver
+Time=13 a=1 b=0 sum=0 carry=0
+Monitor
+Time=14 a=1 b=0 sum=1 carry=0
+Scoreboard
+Time=14 a=1 b=0 sum=1 carry=0
+-----PASS-----
+Simulation complete via implicit call to $finish(1) at time 15 NS + 1
+./Test.sv:2 program test(inter interf);
+xcelium> exit
