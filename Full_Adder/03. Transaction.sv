195fec470689428545c7901b73f571ce41a4461f
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-07 17:26:42 +0530

    Create 03. Transaction.sv

---
+++ Full_Adder/03. Transaction.sv
@@ -0,0 +1,11 @@
+class transaction;
+  rand bit a;
+  rand bit b;
+  rand bit c;
+  bit sum;
+  bit carry;
+  function void display(string str);
+    $display("%s",str);
+    $display("Time=%0t a=%0b b=%0b c=%0b sum=%0b carry=%0b",$time,a,b,c,sum,carry);
+  endfunction
+endclass
