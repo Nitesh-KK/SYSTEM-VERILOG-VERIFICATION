ad794c0c19c59717441da847f408c78b7c7dfcd2
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-17 19:29:26 +0530

    Create 08. Scoreboard.sv

---
+++ D-Flip Flop/08. Scoreboard.sv
@@ -0,0 +1,19 @@
+class scoreboard;
+  transaction trans;
+  mailbox mon_to_score;
+  event e1;
+  
+  function new(mailbox mon_to_score);
+    this.mon_to_score=mon_to_score;
+  endfunction
+  task run();
+    repeat(50) begin
+      mon_to_score.get(trans);
+      trans.display("Scoreboard");
+      if(((trans.rst==1)&&(trans.q==0))||((trans.rst==0)&&(trans.q==trans.d)))
+        $display("-----PASS-----");
+      else $display("-----FAIL-----");
+      -> e1;
+    end
+  endtask
+endclass
