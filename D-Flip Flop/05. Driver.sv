e3371d5dde4cbc2ed870abd39f08f216919f3aad
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-17 19:26:14 +0530

    Create 05. Driver.sv

---
+++ D-Flip Flop/05. Driver.sv
@@ -0,0 +1,21 @@
+class driver;
+  transaction trans;
+  virtual inter interf;
+  mailbox gen_to_driv;
+  
+  function new(virtual inter interf,mailbox gen_to_driv);
+    this.interf=interf;
+    this.gen_to_driv=gen_to_driv;
+  endfunction
+  
+  task run();
+    trans=new();
+    repeat(50) begin
+      gen_to_driv.get(trans);
+      @(negedge interf.clk) begin
+        interf.d<=trans.d;
+        trans.display("Driver");
+      end
+    end
+  endtask
+endclass
