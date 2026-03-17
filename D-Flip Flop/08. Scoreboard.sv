91ae87ef56ac648158ac351fbdaa5a022e9ccb43
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-17 19:35:18 +0530

    Update 08. Scoreboard.sv

--- D-Flip Flop/08. Scoreboard.sv
+++ D-Flip Flop/08. Scoreboard.sv
@@ -7,7 +7,7 @@ class scoreboard;
     this.mon_to_score=mon_to_score;
   endfunction
   task run();
-    repeat(50) begin
+    repeat(30) begin
       mon_to_score.get(trans);
       trans.display("Scoreboard");
       if(((trans.rst==1)&&(trans.q==0))||((trans.rst==0)&&(trans.q==trans.d)))
