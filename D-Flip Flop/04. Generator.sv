9c828ab2a649e1e38c70f09b2f35654b612f64e1
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-17 19:25:44 +0530

    Create 04. Generator.sv

---
+++ D-Flip Flop/04. Generator.sv
@@ -0,0 +1,20 @@
+class generator;
+  mailbox gen_to_driv;
+  transaction trans;
+  event e1;
+  
+  function new(mailbox gen_to_driv);
+    this.gen_to_driv=gen_to_driv;
+  endfunction
+  
+  task run();
+    this.trans=trans;
+    repeat(50) begin
+      trans=new();
+      void'(trans.randomize());
+      trans.display("Generator");
+      gen_to_driv.put(trans);
+      @e1;
+    end
+  endtask
+endclass
