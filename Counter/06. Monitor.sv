7ae1e1026e77b7729fea01f4cc84444652f11ac6
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-19 19:06:27 +0530

    Create 06. Monitor.sv

---
+++ Counter/06. Monitor.sv
@@ -0,0 +1,23 @@
+class monitor;
+  mailbox mon_to_score;
+  transaction trans;
+  virtual inter interf;
+  
+  function new(virtual inter interf,mailbox mon_to_score);
+    this.interf=interf;
+    this.mon_to_score=mon_to_score;
+  endfunction
+  
+  task run();
+    repeat(25) begin
+      trans=new();
+      @(posedge interf.clk) begin
+        trans.rst=interf.rst;
+        trans.up=interf.up;
+        trans.count=interf.count;
+        mon_to_score.put(trans);
+        trans.display("Monitor");
+      end
+    end
+  endtask
+endclass
