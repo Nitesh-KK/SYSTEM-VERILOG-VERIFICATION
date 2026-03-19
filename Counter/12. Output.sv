9aafdfea3c1d293fb7bb4c87102220e081a59dfc
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-19 19:09:55 +0530

    Create 12. Output.sv

---
+++ Counter/12. Output.sv
@@ -0,0 +1,234 @@
+OUTPUT:
+xrun: 25.03-s001: (c) Copyright 1995-2025 Cadence Design Systems, Inc.
+	Top level design units:
+		$unit_0x67f934e9
+		tb
+Loading snapshot worklib.tb:sv .................... Done
+SVSEED default: 1
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+Generator
+Time=0	 rst=0	 up=1	 count=0
+Driver
+Time=5	 rst=0	 up=1	 count=0
+Monitor
+Time=5	 rst=0	 up=1	 count=0
+Scpreboard
+Time=5	 rst=0	 up=1	 count=0
+-----Upcount failed-----
+Generator
+Time=5	 rst=0	 up=1	 count=0
+Monitor
+Time=15	 rst=0	 up=1	 count=1
+Driver
+Time=15	 rst=0	 up=1	 count=0
+Scpreboard
+Time=15	 rst=0	 up=1	 count=1
+-----Upcount passed-----
+Generator
+Time=15	 rst=0	 up=0	 count=0
+Monitor
+Time=25	 rst=0	 up=1	 count=2
+Driver
+Time=25	 rst=0	 up=0	 count=0
+Scpreboard
+Time=25	 rst=0	 up=1	 count=2
+-----Upcount passed-----
+Generator
+Time=25	 rst=0	 up=1	 count=0
+Monitor
+Time=35	 rst=0	 up=0	 count=1
+Driver
+Time=35	 rst=0	 up=1	 count=0
+Scpreboard
+Time=35	 rst=0	 up=0	 count=1
+-----Downcount passed-----
+Generator
+Time=35	 rst=0	 up=1	 count=0
+Monitor
+Time=45	 rst=0	 up=1	 count=2
+Driver
+Time=45	 rst=0	 up=1	 count=0
+Scpreboard
+Time=45	 rst=0	 up=1	 count=2
+-----Upcount passed-----
+Generator
+Time=45	 rst=0	 up=1	 count=0
+Monitor
+Time=55	 rst=0	 up=1	 count=3
+Driver
+Time=55	 rst=0	 up=1	 count=0
+Scpreboard
+Time=55	 rst=0	 up=1	 count=3
+-----Upcount passed-----
+Generator
+Time=55	 rst=0	 up=1	 count=0
+Monitor
+Time=65	 rst=0	 up=1	 count=4
+Driver
+Time=65	 rst=0	 up=1	 count=0
+Scpreboard
+Time=65	 rst=0	 up=1	 count=4
+-----Upcount passed-----
+Generator
+Time=65	 rst=0	 up=1	 count=0
+Monitor
+Time=75	 rst=0	 up=1	 count=5
+Driver
+Time=75	 rst=0	 up=1	 count=0
+Scpreboard
+Time=75	 rst=0	 up=1	 count=5
+-----Upcount passed-----
+Generator
+Time=75	 rst=0	 up=0	 count=0
+Monitor
+Time=85	 rst=0	 up=1	 count=6
+Driver
+Time=85	 rst=0	 up=0	 count=0
+Scpreboard
+Time=85	 rst=0	 up=1	 count=6
+-----Upcount passed-----
+Generator
+Time=85	 rst=0	 up=0	 count=0
+Monitor
+Time=95	 rst=0	 up=0	 count=5
+Driver
+Time=95	 rst=0	 up=0	 count=0
+Scpreboard
+Time=95	 rst=0	 up=0	 count=5
+-----Downcount passed-----
+Generator
+Time=95	 rst=0	 up=1	 count=0
+Monitor
+Time=105	 rst=0	 up=0	 count=4
+Driver
+Time=105	 rst=0	 up=1	 count=0
+Scpreboard
+Time=105	 rst=0	 up=0	 count=4
+-----Downcount passed-----
+Generator
+Time=105	 rst=0	 up=1	 count=0
+Monitor
+Time=115	 rst=0	 up=1	 count=5
+Driver
+Time=115	 rst=0	 up=1	 count=0
+Scpreboard
+Time=115	 rst=0	 up=1	 count=5
+-----Upcount passed-----
+Generator
+Time=115	 rst=0	 up=0	 count=0
+Monitor
+Time=125	 rst=0	 up=1	 count=6
+Driver
+Time=125	 rst=0	 up=0	 count=0
+Scpreboard
+Time=125	 rst=0	 up=1	 count=6
+-----Upcount passed-----
+Generator
+Time=125	 rst=0	 up=0	 count=0
+Monitor
+Time=135	 rst=0	 up=0	 count=5
+Driver
+Time=135	 rst=0	 up=0	 count=0
+Scpreboard
+Time=135	 rst=0	 up=0	 count=5
+-----Downcount passed-----
+Generator
+Time=135	 rst=0	 up=1	 count=0
+Monitor
+Time=145	 rst=0	 up=0	 count=4
+Driver
+Time=145	 rst=0	 up=1	 count=0
+Scpreboard
+Time=145	 rst=0	 up=0	 count=4
+-----Downcount passed-----
+Generator
+Time=145	 rst=0	 up=1	 count=0
+Monitor
+Time=155	 rst=0	 up=1	 count=5
+Driver
+Time=155	 rst=0	 up=1	 count=0
+Scpreboard
+Time=155	 rst=0	 up=1	 count=5
+-----Upcount passed-----
+Generator
+Time=155	 rst=0	 up=0	 count=0
+Monitor
+Time=165	 rst=0	 up=1	 count=6
+Driver
+Time=165	 rst=0	 up=0	 count=0
+Scpreboard
+Time=165	 rst=0	 up=1	 count=6
+-----Upcount passed-----
+Generator
+Time=165	 rst=0	 up=0	 count=0
+Monitor
+Time=175	 rst=0	 up=0	 count=5
+Driver
+Time=175	 rst=0	 up=0	 count=0
+Scpreboard
+Time=175	 rst=0	 up=0	 count=5
+-----Downcount passed-----
+Generator
+Time=175	 rst=0	 up=0	 count=0
+Monitor
+Time=185	 rst=0	 up=0	 count=4
+Driver
+Time=185	 rst=0	 up=0	 count=0
+Scpreboard
+Time=185	 rst=0	 up=0	 count=4
+-----Downcount passed-----
+Generator
+Time=185	 rst=0	 up=1	 count=0
+Monitor
+Time=195	 rst=0	 up=0	 count=3
+Driver
+Time=195	 rst=0	 up=1	 count=0
+Scpreboard
+Time=195	 rst=0	 up=0	 count=3
+-----Downcount passed-----
+Generator
+Time=195	 rst=0	 up=0	 count=0
+Monitor
+Time=205	 rst=0	 up=1	 count=4
+Driver
+Time=205	 rst=0	 up=0	 count=0
+Scpreboard
+Time=205	 rst=0	 up=1	 count=4
+-----Upcount passed-----
+Generator
+Time=205	 rst=0	 up=1	 count=0
+Monitor
+Time=215	 rst=0	 up=0	 count=3
+Driver
+Time=215	 rst=0	 up=1	 count=0
+Scpreboard
+Time=215	 rst=0	 up=0	 count=3
+-----Downcount passed-----
+Generator
+Time=215	 rst=0	 up=1	 count=0
+Monitor
+Time=225	 rst=0	 up=1	 count=4
+Driver
+Time=225	 rst=0	 up=1	 count=0
+Scpreboard
+Time=225	 rst=0	 up=1	 count=4
+-----Upcount passed-----
+Generator
+Time=225	 rst=0	 up=1	 count=0
+Monitor
+Time=235	 rst=0	 up=1	 count=5
+Driver
+Time=235	 rst=0	 up=1	 count=0
+Scpreboard
+Time=235	 rst=0	 up=1	 count=5
+-----Upcount passed-----
+Generator
+Time=235	 rst=0	 up=1	 count=0
+Monitor
+Time=245	 rst=0	 up=1	 count=6
+Driver
+Time=245	 rst=0	 up=1	 count=0
+Scpreboard
+Time=245	 rst=0	 up=1	 count=6
+-----Upcount passed-----
