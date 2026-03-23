9b79ea5245efe56caf8d57c7f98b4fba18159e69
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-23 18:02:04 +0530

    Create 05. Driver.sv

---
+++ Syncronous FIFO/05. Driver.sv
@@ -0,0 +1,24 @@
+class driver;
+  mailbox gen_to_drive;
+  transaction trans;
+  virtual inter interf;
+  
+  function new(mailbox gen_to_drive,virtual inter interf);
+    this.gen_to_drive=gen_to_drive;
+    this.interf=interf;
+  endfunction
+  
+  task run();
+    trans=new();
+    repeat(25) begin
+      gen_to_drive.get(trans);
+      @(posedge interf.clk) begin
+      	interf.rst=trans.rst;
+      	interf.re=trans.re;
+        interf.we=trans.we;
+        interf.din=trans.din;
+      	trans.display("Driver");
+      end
+    end
+  endtask
+endclass
