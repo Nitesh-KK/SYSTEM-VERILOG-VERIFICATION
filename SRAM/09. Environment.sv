1dce66124374cbdddd46aba4431b5522a278a7d6
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-24 17:12:58 +0530

    Create 09. Environment.sv

---
+++ SRAM/09. Environment.sv
@@ -0,0 +1,22 @@
+`include "Agent.sv"
+`include "Scoreboard.sv"
+
+class environment;
+  agent age;
+  scoreboard score;
+  virtual inter interf;
+  mailbox mon_to_score;
+  function new(virtual inter interf);
+    this.interf=interf;
+    mon_to_score=new();
+    age=new(mon_to_score,interf);
+    score=new(mon_to_score);
+    score.e1=age.gen.e1;
+  endfunction
+  task test();
+    fork
+      age.run();
+      score.run();
+    join
+  endtask
+endclass
