44e2df24a7a02d95f0b076fd6be9a37e28b8b47f
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-24 17:15:01 +0530

    Create 12. Output.sv

---
+++ SRAM/12. Output.sv
@@ -0,0 +1,227 @@
+OUTPUT:
+xcelium> run
+Generator
+time=0	rst=0	 en=1	 din=16	 addr=20	 dout=0
+Driver
+time=5	rst=0	 en=1	 din=16	 addr=20	 dout=0
+Monitor
+time=5	rst=0	 en=1	 din=16	 addr=20	 dout=0
+Scoreboard
+time=5	rst=0	 en=1	 din=16	 addr=20	 dout=0
+-----Write pass-----
+Generator
+time=5	rst=0	 en=1	 din=17	 addr=28	 dout=0
+Monitor
+time=15	rst=0	 en=1	 din=16	 addr=20	 dout=0
+Driver
+time=15	rst=0	 en=1	 din=17	 addr=28	 dout=0
+Scoreboard
+time=15	rst=0	 en=1	 din=16	 addr=20	 dout=0
+-----Write pass-----
+Generator
+time=15	rst=0	 en=0	 din=2	 addr=20	 dout=0
+Monitor
+time=25	rst=0	 en=1	 din=17	 addr=28	 dout=0
+Driver
+time=25	rst=0	 en=0	 din=2	 addr=20	 dout=0
+Scoreboard
+time=25	rst=0	 en=1	 din=17	 addr=28	 dout=0
+-----Write pass-----
+Generator
+time=25	rst=0	 en=1	 din=28	 addr=17	 dout=0
+Monitor
+time=35	rst=0	 en=0	 din=2	 addr=20	 dout=16
+Driver
+time=35	rst=0	 en=1	 din=28	 addr=17	 dout=0
+Scoreboard
+time=35	rst=0	 en=0	 din=2	 addr=20	 dout=16
+-----Read pass-----
+Generator
+time=35	rst=0	 en=1	 din=29	 addr=25	 dout=0
+Monitor
+time=45	rst=0	 en=1	 din=28	 addr=17	 dout=16
+Driver
+time=45	rst=0	 en=1	 din=29	 addr=25	 dout=0
+Scoreboard
+time=45	rst=0	 en=1	 din=28	 addr=17	 dout=16
+-----Write pass-----
+Generator
+time=45	rst=0	 en=1	 din=6	 addr=17	 dout=0
+Monitor
+time=55	rst=0	 en=1	 din=29	 addr=25	 dout=16
+Driver
+time=55	rst=0	 en=1	 din=6	 addr=17	 dout=0
+Scoreboard
+time=55	rst=0	 en=1	 din=29	 addr=25	 dout=16
+-----Write pass-----
+Generator
+time=55	rst=0	 en=1	 din=25	 addr=5	 dout=0
+Monitor
+time=65	rst=0	 en=1	 din=6	 addr=17	 dout=16
+Driver
+time=65	rst=0	 en=1	 din=25	 addr=5	 dout=0
+Scoreboard
+time=65	rst=0	 en=1	 din=6	 addr=17	 dout=16
+-----Write pass-----
+Generator
+time=65	rst=0	 en=1	 din=7	 addr=7	 dout=0
+Monitor
+time=75	rst=0	 en=1	 din=25	 addr=5	 dout=16
+Driver
+time=75	rst=0	 en=1	 din=7	 addr=7	 dout=0
+Scoreboard
+time=75	rst=0	 en=1	 din=25	 addr=5	 dout=16
+-----Write pass-----
+Generator
+time=75	rst=0	 en=0	 din=4	 addr=2	 dout=0
+Monitor
+time=85	rst=0	 en=1	 din=7	 addr=7	 dout=16
+Driver
+time=85	rst=0	 en=0	 din=4	 addr=2	 dout=0
+Scoreboard
+time=85	rst=0	 en=1	 din=7	 addr=7	 dout=16
+-----Write pass-----
+Generator
+time=85	rst=0	 en=0	 din=9	 addr=19	 dout=0
+Monitor
+time=95	rst=0	 en=0	 din=4	 addr=2	 dout=0
+Driver
+time=95	rst=0	 en=0	 din=9	 addr=19	 dout=0
+Scoreboard
+time=95	rst=0	 en=0	 din=4	 addr=2	 dout=0
+-----Read pass-----
+Generator
+time=95	rst=0	 en=1	 din=29	 addr=25	 dout=0
+Monitor
+time=105	rst=0	 en=0	 din=9	 addr=19	 dout=0
+Driver
+time=105	rst=0	 en=1	 din=29	 addr=25	 dout=0
+Scoreboard
+time=105	rst=0	 en=0	 din=9	 addr=19	 dout=0
+-----Read pass-----
+Generator
+time=105	rst=0	 en=0	 din=26	 addr=14	 dout=0
+Monitor
+time=115	rst=0	 en=1	 din=29	 addr=25	 dout=0
+Driver
+time=115	rst=0	 en=0	 din=26	 addr=14	 dout=0
+Scoreboard
+time=115	rst=0	 en=1	 din=29	 addr=25	 dout=0
+-----Write pass-----
+Generator
+time=115	rst=0	 en=0	 din=23	 addr=11	 dout=0
+Monitor
+time=125	rst=0	 en=0	 din=26	 addr=14	 dout=0
+Driver
+time=125	rst=0	 en=0	 din=23	 addr=11	 dout=0
+Scoreboard
+time=125	rst=0	 en=0	 din=26	 addr=14	 dout=0
+-----Read pass-----
+Generator
+time=125	rst=0	 en=0	 din=11	 addr=5	 	 dout=0
+Monitor
+time=135	rst=0	 en=0	 din=23	 addr=11	 dout=0
+Driver
+time=135	rst=0	 en=0	 din=11	 addr=5	 	 dout=0
+Scoreboard
+time=135	rst=0	 en=0	 din=23	 addr=11	 dout=0
+-----Read pass-----
+Generator
+time=135	rst=0	 en=1	 din=16	 addr=31	 dout=0
+Monitor
+time=145	rst=0	 en=0	 din=11	 addr=5	 dout=25
+Driver
+time=145	rst=0	 en=1	 din=16	 addr=31	 dout=0
+Scoreboard
+time=145	rst=0	 en=0	 din=11	 addr=5	 dout=25
+-----Read pass-----
+Generator
+time=145	rst=0	 en=1	 din=3	 addr=29	 dout=0
+Monitor
+time=155	rst=0	 en=1	 din=16	 addr=31	 dout=25
+Driver
+time=155	rst=0	 en=1	 din=3	 addr=29	 dout=0
+Scoreboard
+time=155	rst=0	 en=1	 din=16	 addr=31	 dout=25
+-----Write pass-----
+Generator
+time=155	rst=0	 en=0	 din=8	 addr=10	 dout=0
+Monitor
+time=165	rst=0	 en=1	 din=3	 addr=29	 dout=25
+Driver
+time=165	rst=0	 en=0	 din=8	 addr=10	 dout=0
+Scoreboard
+time=165	rst=0	 en=1	 din=3	 addr=29	 dout=25
+-----Write pass-----
+Generator
+time=165	rst=0	 en=0	 din=22	 addr=23	 dout=0
+Monitor
+time=175	rst=0	 en=0	 din=8	 addr=10	 dout=0
+Driver
+time=175	rst=0	 en=0	 din=22	 addr=23	 dout=0
+Scoreboard
+time=175	rst=0	 en=0	 din=8	 addr=10	 dout=0
+-----Read pass-----
+Generator
+time=175	rst=0	 en=0	 din=6	 addr=18	 dout=0
+Monitor
+time=185	rst=0	 en=0	 din=22	 addr=23	 dout=0
+Driver
+time=185	rst=0	 en=0	 din=6	 addr=18	 dout=0
+Scoreboard
+time=185	rst=0	 en=0	 din=22	 addr=23	 dout=0
+-----Read pass-----
+Generator
+time=185	rst=0	 en=1	 din=29	 addr=12	 dout=0
+Monitor
+time=195	rst=0	 en=0	 din=6	 addr=18	 dout=0
+Driver
+time=195	rst=0	 en=1	 din=29	 addr=12	 dout=0
+Scoreboard
+time=195	rst=0	 en=0	 din=6	 addr=18	 dout=0
+-----Read pass-----
+Generator
+time=195	rst=0	 en=0	 din=24	 addr=30	 dout=0
+Monitor
+time=205	rst=0	 en=1	 din=29	 addr=12	 dout=0
+Driver
+time=205	rst=0	 en=0	 din=24	 addr=30	 dout=0
+Scoreboard
+time=205	rst=0	 en=1	 din=29	 addr=12	 dout=0
+-----Write pass-----
+Generator
+time=205	rst=0	 en=1	 din=29	 addr=12	 dout=0
+Monitor
+time=215	rst=0	 en=0	 din=24	 addr=30	 dout=0
+Driver
+time=215	rst=0	 en=1	 din=29	 addr=12	 dout=0
+Scoreboard
+time=215	rst=0	 en=0	 din=24	 addr=30	 dout=0
+-----Read pass-----
+Generator
+time=215	rst=0	 en=1	 din=0	 addr=2	 dout=0
+Monitor
+time=225	rst=0	 en=1	 din=29	 addr=12	 dout=0
+Driver
+time=225	rst=0	 en=1	 din=0	 addr=2	 dout=0
+Scoreboard
+time=225	rst=0	 en=1	 din=29	 addr=12	 dout=0
+-----Write pass-----
+Generator
+time=225	rst=0	 en=1	 din=17	 addr=5	 dout=0
+Monitor
+time=235	rst=0	 en=1	 din=0	 addr=2	 dout=0
+Driver
+time=235	rst=0	 en=1	 din=17	 addr=5	 dout=0
+Scoreboard
+time=235	rst=0	 en=1	 din=0	 addr=2	 dout=0
+-----Write pass-----
+Generator
+time=235	rst=0	 en=1	 din=26	 addr=11	 dout=0
+Monitor
+time=245	rst=0	 en=1	 din=17	 addr=5	 dout=0
+Driver
+time=245	rst=0	 en=1	 din=26	 addr=11	 dout=0
+Scoreboard
+time=245	rst=0	 en=1	 din=17	 addr=5	 dout=0
+-----Write pass-----
